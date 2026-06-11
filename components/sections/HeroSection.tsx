d425289cdd1ca62532a7b63d20c02463e5c16395
Yohann <yoyo24du26@gmail.com>
2026-06-11 14:38:45 +0200

    fix(Hero section): fixing some details

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import { redirect } from 'next/navigation'
-
-// Background components
+// Decorative components
 import HeroBackground from "@/components/decorative/HeroBackground";
 
 // UI components
+import { Badge } from "@/components/ui/badge";
 import Cta from "@/components/ui/cta";
 
 // Animation components
 import FadeContent from "@/components/FadeContent";
 import SplitText from "@/components/SplitText";
-import { Badge } from "../ui/badge";
 
 // Icons
 import { ArrowDown, Briefcase } from "lucide-react";
@@ -22,14 +20,19 @@ export default function HeroSection() {
   return (
     <section className="relative h-dvh">
       <div className="relative flex flex-col gap-8 justify-center w-full h-full px-5 sm:px-20 z-1">
-        <FadeContent duration={1} delay={1.5} className="flex items-center gap-4">
+        <FadeContent
+          duration={1}
+          delay={1.5}
+          className="flex items-center gap-4"
+        >
           <div className="flex flex-col">
-            <p>
-              <span className='underline'>Yohann RENAULD</span><br/>
+            <p className="text-xl text-shadow-[0_0_5px_var(--background)]">
+              <span className="underline">Yohann RENAULD</span>
+              <br />
               Portfolio {date.getFullYear()}
             </p>
             <Badge variant="outline" className="relative mt-1 overflow-visible">
-              <Briefcase data-icon="inline-start"/>
+              <Briefcase data-icon="inline-start" />
               Open to work
               <span className="absolute inset-x-[23] inset-y-[1] block bg-primary -z-1 rounded-full animate-ping"></span>
             </Badge>
@@ -53,11 +56,13 @@ export default function HeroSection() {
         <FadeContent duration={1} delay={1.5}>
           <div className="flex flex-col items-center justify-end lg:flex-row gap-10">
             <p className="p-1 w-fit rounded-sm">
-              Étant passionné de création visuel et d'animations,<br />
+              Étant passionné de création visuel et d'animations,
+              <br />
               je suis ouvert à tous type de projets.
             </p>
             <Cta size="xxl">
-              <ArrowDown className="size-5 mt-1 animate-bounce" />Catégories
+              <ArrowDown className="size-5 mt-1 animate-bounce" />
+              Catégories
             </Cta>
           </div>
         </FadeContent>
