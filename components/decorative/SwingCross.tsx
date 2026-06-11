0bbcb4922e900d86189c7ac83743b52314828733
Yohann <yoyo24du26@gmail.com>
2026-06-11 14:37:25 +0200

    feat(): creating - `SwingCross` decorative component - `anchor` ui component - `FlowingMenu` animated component

---
+++ components/decorative/SwingCross.tsx
@@ -0,0 +1,7 @@
+import { Plus } from "lucide-react";
+
+export default function SwingCross() {
+  return (
+    <Plus />
+  );
+}
---
+++ components/ui/anchor.tsx
@@ -0,0 +1,40 @@
+import Link, { type LinkProps } from "next/link";
+
+type AnchorProps = LinkProps &
+  React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+    children: React.ReactNode;
+    className?: string;
+  };
+
+export default function Anchor({
+  href,
+  children,
+  className = "",
+  ...props
+}: AnchorProps) {
+  return (
+    <Link
+      href={href}
+      className={`
+        relative
+        p-0.5
+        after:content-['']
+        after:bg-primary
+        after:rounded-[4px]
+        after:block
+        after:absolute
+        after:inset-[calc(100%-.125rem)_0_0_0]
+        after:w-full
+        after:-z-1
+        after:duration-200
+        hover:after:bg-secondary
+        hover:after:inset-[-.125rem_0_0_0]
+        hover:after:ease-[cubic-bezier(.01,0,.19,2.36)]
+        ${className}
+      `}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+}
