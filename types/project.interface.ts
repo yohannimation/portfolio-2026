73e0f277aba10166762c7fa6bbffc5d59f1dfafd
Yohann <yoyo24du26@gmail.com>
2026-06-27 19:32:27 +0200

    fix(Interface): adding `TagInterface` & upgrading `ProjectInterface`

--- types/project.interface.ts
+++ types/project.interface.ts
@@ -1,3 +1,13 @@
+import type { TagInterface } from "./tag.interface";
+
 export interface ProjectInterface {
+    id: number;
     name: string;
+    description: string;
+    source: string;
+    sourceType: string;
+    miniatureFile: File;
+    miniatureName: string;
+    active: boolean;
+    tags: TagInterface[];
 }
