d2af81c9fb9da67e60a482334984b68d76262e58
Yohann <yoyo24du26@gmail.com>
2026-07-05 16:08:51 +0200

    feat(backend): implementing - `nelmio/cors-bundle`to prevent CORS error - activating the Symfony serializer - creating `AbstractApiController`

--- apps/backend/config/bundles.php
+++ apps/backend/config/bundles.php
@@ -17,4 +17,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
