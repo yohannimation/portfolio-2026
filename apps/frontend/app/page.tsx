3477fbbb73bff1c769d8a296b86ea574c9228387
Yohann <yoyo24du26@gmail.com>
2026-07-16 09:35:09 +0200

    refactor(frontend/HomePage): reorder imports

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,11 +1,12 @@
-import { categoryService } from "@/services/category.service";
-
 // Section components
 import CategorySection from "@/components/sections/Home/CategorySection";
 import CompetencySection from "@/components/sections/Home/CompetencySection";
 import HeroSection from "@/components/sections/Home/HeroSection";
 import MoreSection from "@/components/sections/Home/MoreSection";
 import ProfileSection from "@/components/sections/Home/ProfileSection";
+
+// Services
+import { categoryService } from "@/services/category.service";
 
 export default async function Home() {
   const categories = await categoryService.getCategories();
