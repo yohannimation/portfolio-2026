483044eedea78ef996e8ceeb99b448d0b7b168df
Yohann <yoyo24du26@gmail.com>
2026-08-20 09:36:19 +0200

    feat(frontend/sitemap): adding `sitemap` generation

---
+++ apps/frontend/app/sitemap.ts
@@ -0,0 +1,33 @@
+import type { MetadataRoute } from 'next'
+
+// Services
+import { categoryService } from "@/services/category.service";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = "https://yohannimation.fr";
+  const lastmodTime = new Date(2026, 7, 20, 0, 0);
+
+  const categories = await categoryService.getCategories();
+  const categoryUrls: MetadataRoute.Sitemap = categories.map((category) => ({
+    url: `${baseUrl}/category/${category.id}-${category.slug}`,
+    lastModified: lastmodTime,
+    changeFrequency: 'monthly',
+    priority: 0.8,
+  }))
+
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: lastmodTime,
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    ...categoryUrls,
+    {
+      url: `${baseUrl}/about-me`,
+      lastModified: lastmodTime,
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ]
+}
