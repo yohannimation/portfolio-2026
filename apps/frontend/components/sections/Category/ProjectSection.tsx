eed522ba829a42cc21a887b42859f541624592ef
Yohann <yoyo24du26@gmail.com>
2026-08-20 14:13:54 +0200

    feat(frontend/CategoryPage): adding separator between projects

--- apps/frontend/components/sections/Category/ProjectSection.tsx
+++ apps/frontend/components/sections/Category/ProjectSection.tsx
@@ -13,10 +13,20 @@ interface ProjectSectionInterface {
 export default function ProjectSection({ projects }: ProjectSectionInterface) {
   return (
     <section id="project" className="mb-10 md:mb-20">
-      <ul className="flex flex-col gap-8">
+      <ul className="flex flex-col">
         {projects?.map((project, index) => (
-          <li key={project.id}>
+          <li
+            key={project.id}
+            className="flex flex-col gap-8 pt-8 first:pt-0 last:pb-0"
+          >
             <LinkProject project={project} index={index} />
+
+            {index < (projects?.length ?? 0) - 1 && (
+              <span
+                className="h-px w-[70%] lg:w-[50%] mx-auto bg-primary"
+                aria-hidden="true"
+              />
+            )}
           </li>
         ))}
       </ul>
