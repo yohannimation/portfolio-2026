5c38911ee97e18ffa6af252e89da111c249acf23
Yohann <yoyo24du26@gmail.com>
2026-09-03 11:50:19 +0200

    feat(frontend): adding a custom cursor

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import localFont from 'next/font/local'
 
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
+import { SmoothCursor } from "@/components/ui/smooth-cursor";
 
 import "./globals.css";
 
@@ -65,6 +66,7 @@ export default function RootLayout({
         bg-background/50
         shadow-[0_0_50px_rgba(56,132,35,0.15),0_0_20px_rgba(56,132,35,0.2),0_0_15px_rgba(56,132,35,0.3)]
       ">
+        <SmoothCursor />
         <Header />
         <main className="relative bg-background min-h-screen z-1">
           {children}
