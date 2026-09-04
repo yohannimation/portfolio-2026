8724cbe8203bf7b425fe1934eb8539f73943b6f2
Yohann <yoyo24du26@gmail.com>
2026-09-04 13:23:09 +0200

    Revert "feat(frontend/CategoryPage): adding project reveal animation triggered from the scroll"
    This reverts commit 2f69ec3352d79e00af279c7d1ae4fa35403b9252.

--- apps/frontend/components/projects/LinkProject.tsx
+++ apps/frontend/components/projects/LinkProject.tsx
@@ -3,9 +3,6 @@ import ProjectContent from "@/components/projects/ProjectContent";
 import ProjectHeader from "@/components/projects/ProjectHeader";
 import ProjectMiniature from "@/components/projects/ProjectMiniature";
 
-// Animation components
-import AnimatedContent from "../AnimatedContent";
-
 // Libs
 import slugify from "slugify";
 
@@ -25,27 +22,21 @@ export default function LinkProject({ project, index }: LinkProjectInterface) {
   const isOdd = index % 2 === 1;
   
   return (
-    <AnimatedContent
-      duration={0.8}
-      ease="expo.out"
-      delay={0.25}
-    >
-      <article id={articleId}>
-        <ProjectHeader name={project.name} date={project.active ? project.date : ""} />
+    <article id={articleId}>
+      <ProjectHeader name={project.name} date={project.active ? project.date : ""} />
 
-        <div className={`flex gap-4 flex-col ${isOdd ? "md:flex-row-reverse" : "md:flex-row"}`}>
-          <ProjectMiniature
-            active={project.active}
-            name={project.name}
-            miniatureName={project.miniatureName}
-            sourceType={project.sourceType}
-            source={project.source}
-            tags={project.tags}
-            isOdd={isOdd}
-          />
-          <ProjectContent type={project.type} description={project.description} />
-        </div>
-      </article>
-    </AnimatedContent>
+      <div className={`flex gap-4 flex-col ${isOdd ? "md:flex-row-reverse" : "md:flex-row"}`}>
+        <ProjectMiniature
+          active={project.active}
+          name={project.name}
+          miniatureName={project.miniatureName}
+          sourceType={project.sourceType}
+          source={project.source}
+          tags={project.tags}
+          isOdd={isOdd}
+        />
+        <ProjectContent type={project.type} description={project.description} />
+      </div>
+    </article>
   );
 }
